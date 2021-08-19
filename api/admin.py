2af25257dec93cbb445837120706821aea274ccc
tssaad <tssaad.g@gmail.com>
2021-08-19 20:39:39 +0200

    APIs

---
+++ api/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Meal, Rating
+
+
+
+class AdminMeal(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    search_fields = ['title', 'description']
+    list_filter = ['title', 'description']
+
+admin.site.register(Meal, AdminMeal)
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'meal', 'user', 'stars']
+    list_filter = ['meal', 'user']
+    
+admin.site.register(Rating)
